a3bf86ff0cf598b5c8f850830dde7a58ee68b5be
Danijel Tasov <dt@korn.shell.la>
2009-11-25 20:49:15 +0100

    cleaned up documentation

--- lib/Nagios/Passive.pm
+++ lib/Nagios/Passive.pm
@@ -1,5 +1,5 @@
 package Nagios::Passive;
-use Class::MOP;
+require Class::MOP;
 
 sub create {
   my $this = shift;
@@ -15,3 +15,65 @@ sub create {
   return $class->new(%opts);
 }
 1;
+__END__
+
+=head1 NAME
+
+Nagios::Passive - drop check results into Nagios' check_result_path.
+
+=head1 SYNOPSIS
+
+  my $nw = Nagios::Passive->create(
+    checkresults_dir => $checkresultsdir,
+    service_description => $service_description,
+    check_name => $check_name,
+    host_name  => $hostname,
+    return_code => 0, # 1 2 3 
+    output => 'looks (good|bad|horrible) | performancedata'
+  );
+  $nw->submit;
+
+=head1 DESCRIPTION
+
+This is the factory class, currently it creates either a
+Nagios::Passive::CommandFile or a Nagios::Passive::ResultPath object.
+Which object is created depends on the keys you supply to the the create
+method. The common interface to of the resulting objects is documented
+in L<Nagios::Passive::Base>.
+
+=head1 METHODS
+
+=head2 create( %ARGS )
+
+This method returns either a Nagios::Passive::CommandFile or
+a Nagios::Passive::ResultPath object. If there is a key named
+C<checkresults_dir> then a N::P::ResultPath object is created.
+If there is a key C<command_file> a N::P::CommandFile object is
+created.
+
+=head1 LIMITATIONS
+
+This module is in an early stage of development, the API is
+likely to brake in the future.
+
+Also it interacts with an undocumented feature of Nagios. This
+feature may disappear in the future.
+
+=head1 DEVELOPMENT
+
+Development takes place on github:
+
+L<http://github.com/datamuc/Nagios-Passive>
+
+=head1 AUTHOR
+
+Danijel Tasov, <data@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2009, Danijel Tasov
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
--- lib/Nagios/Passive/CommandFile.pm
+++ lib/Nagios/Passive/CommandFile.pm
@@ -60,136 +60,20 @@ Nagios::Spool::Writer - drop check results into Nagios' check_result_path.
 =head1 SYNOPSIS
 
   my $nw = Nagios::Spool::Writer->new(
-    checkresults_dir => $checkresultsdir,
+    command_file => $checkresultsdir,
     service_description => $service_description,
     check_name => $check_name,
     host_name  => $hostname,
     return_code => 0, # 1 2 3 
     output => 'looks (good|bad|horrible) | performancedata'
   );
-  $nw->write_file;
+  $nw->submit;
 
 =head1 DESCRIPTION
 
-This module gives you the ability to drop checkresults directly
-into Nagios' check_result_path.
+This module gives you the ability to drop checkresults into
+Nagios' external_command_file.
 
-=head1 CONSTRUCTOR
-
-=head2 new( %ARGS )
-
-=over 4
-
-=item checkresults_dir DIRECTORY
-
-The directory where Nagios' configuration option `check_result_path'
-points to.
-
-=item hostname STRING
-
-The hostname on which the check is bound to.
-
-=item service_description STRING
-
-The service description of the check. This is optional, if you
-omit it, the result is treated as a check result for the host
-check of hostname
-
-=item check_name STRING
-
-The name of the check. A nagios check typically returns a line as
-follows:
-
-  CHECKNAME STATUS - MESSAGE | PERFORMANCE_DATA
-
-This method sets the value of CHECKNAME.
-
-=item output STRING
-
-This sets the text after the dash of the nagios output. (see
-check_name).
-
-=item return_code NUMBER
-
-=over 8
-
-=item 0 - OK
-
-=item 1 - WARNING
-
-=item 2 - CRITICAL
-
-=item 3 - UNKNOWN
-
-=back
-
-This also sets the value of STATUS (see check_name).
-It defaults to `0' if omited.
-
-Instead of setting the return_code directly you can use set_thresholds
-and set_status. (See METHODS).
-
-=back
-
-=head1 METHODS
-
-=head2 set_thresholds HASH
-
-This set's up an Nagios::Plugin::Threshold object.
-
-  $ns->set_thresholds(
-     warning => <rangespec>,
-     critical => <rangespec>,
-  );
-
-For details see 
-L<http://nagiosplug.sourceforge.net/developer-guidelines.html#THRESHOLDFORMAT>
-
-=head2 set_status VALUE
-
-Does value checking of VALUE against the threshold object created with
-C<set_thresholds> and sets C<return_code> accordingly.
-
-=head2 add_perf HASH
-
-  $ns->add_perf(
-     label => 'time',
-     value => '0.1',
-     uom   => 's',
-  );
-
-This adds Performance Data to the object. See
-L<Nagios::Plugin::Performance> on how to use this. Finally the
-performance data is appended to the end of C<output>. Can
-be called multiple times to add more performance data.
-
-=head2 write_file
-
-Write the check_result into Nagios' check_result_path.
-
-=head1 LIMITATIONS
-
-This module is in an early stage of development, the API is
-likely to brake in the future.
-
-Also it interacts with an undocumented feature of Nagios. This
-feature may disappear in the future.
-
-=head1 DEVELOPMENT
-
-Development takes place on github:
-
-L<http://github.com/datamuc/Nagios-Spool-Writer>
-
-=head1 AUTHOR
-
-Danijel Tasov, <data@cpan.org>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2009, Danijel Tasov
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+The usage is described in L<Nagios::Passive>
 
 =cut
