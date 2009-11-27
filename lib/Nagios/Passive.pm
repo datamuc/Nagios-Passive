d1e07b480ba9ce92d22b68f3a826f2712b9d24e5
Danijel Tasov <dt@korn.shell.la>
2009-11-27 02:02:42 +0100

    documentation

--- lib/Nagios/Passive.pm
+++ lib/Nagios/Passive.pm
@@ -19,7 +19,7 @@ __END__
 
 =head1 NAME
 
-Nagios::Passive - drop check results into Nagios' check_result_path.
+Nagios::Passive - submit passive check results to nagios
 
 =head1 SYNOPSIS
 
@@ -36,20 +36,92 @@ Nagios::Passive - drop check results into Nagios' check_result_path.
 =head1 DESCRIPTION
 
 This is the factory class, currently it creates either a
-Nagios::Passive::CommandFile or a Nagios::Passive::ResultPath object.
-Which object is created depends on the keys you supply to the the create
-method. The common interface to of the resulting objects is documented
-in L<Nagios::Passive::Base>.
+Nagios::Passive::CommandFile or a Nagios::Passive::ResultPath
+object.  Which object is created depends on the keys of the hash
+you supply to the the create method.
 
-=head1 METHODS
+=head1 FACTORY METHODS
 
 =head2 create( %ARGS )
 
 This method returns either a Nagios::Passive::CommandFile or
-a Nagios::Passive::ResultPath object. If there is a key named
-C<checkresults_dir> then a N::P::ResultPath object is created.
-If there is a key C<command_file> a N::P::CommandFile object is
-created.
+a Nagios::Passive::ResultPath object.
+
+If there is a key named
+
+=over 4
+
+=item * C<checkresults_dir>, a Nagios::Passive::ResultPath
+
+=item * C<command_file>, a Nagios::Passive::CommandFile
+
+=back
+
+object ist created.
+
+Other required keys are C<host_name> and C<check_name>.
+
+C<host_name> is the hostname for which you want to report a check
+result to nagios.
+
+The typical output of a nagios plugin looks like this:
+
+    CHECK_NAME STATUS - MESSAGE
+
+CHECK_NAME is replaced by C<check_name>. MESSAGE is replaced by
+C<output>.
+
+STATUS can either be set by setting C<return_code> to 0,1,2 or 3
+(See nagios documentation for details) or by using the
+C<set_thresholds> and C<set_status> methods.
+
+C<service_description> is optional, if it's omitted the
+check result belongs to a host check.
+
+All of the attributes (except the required ones) can also be set
+afterwards, by calling the setter methods of the same name, i.e.:
+
+  $nw->return_code(0);
+  $nw->output("everything ok");
+  # results to: CHECK_NAME OK - everything ok
+
+=head1 METHODS
+
+On the object you gathered from the C<create> method, you can
+perform the following operations.
+
+=head2 set_thresholds HASH
+
+  $nw->set_thresholds(
+     warning => ':91',
+     critical => ':97',
+  );
+
+This creates a Nagios::Plugin::Threshold object. It can be used
+to set the C<return_code> with C<set_status>.
+
+=head2 set_status VALUE
+
+Sets the C<return_code> according the the threshold object
+created with set_thresholds and the given VALUE. For example:
+
+   $nw->set_thresholds(warning => ':4', critical => ':8');
+   $nw->set_status(6);
+   $nw->output("6 is a warning");
+   # return_code is now 1, and the output shown in nagios will be
+   # CHECK_NAME WARNING - 6 is a warning
+
+=head2 add_perf HASH
+
+This can be used to add performance data to the check result.
+Read L<Nagios::Plugin::Performance> to get the idea of how to use
+this.
+
+=head2 submit
+
+This writes the data out. In case of the CommandFile this will
+write the result into nagios' external_command_file. In case of
+ResultPath this will drop a file into nagios' check_result_path.
 
 =head1 LIMITATIONS
 
