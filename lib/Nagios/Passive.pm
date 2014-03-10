7192234d7f21cb3a5edfe50a0634286d06ce1473
Danijel Tasov <m@rbfh.de>
2014-03-10 22:04:34 +0100

    update version and changes

--- lib/Nagios/Passive.pm
+++ lib/Nagios/Passive.pm
@@ -2,7 +2,7 @@ package Nagios::Passive;
 use strict;
 use Carp;
 use Class::Load qw/load_class/;
-use version; our $VERSION = qv('0.3.5');
+use version; our $VERSION = qv('0.3.6');
 
 sub create {
   my $this = shift;
